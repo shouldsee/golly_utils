7e1d401a61dfe977e02bf4b7be24a3b48e66be87
shouldsee <shouldsee.gem@gmail.com>
2017-02-17 16:00:21 +0000

    update

---
+++ set_TCA_canonlised.py
@@ -0,0 +1,26 @@
+## This script generate an ECA rule and emulate it on a torus of width 200. 
+## Written by Feng (shouldsee.gem@gmail.com) Feb 2017.
+import golly
+rnum=golly.getstring('TCA number','6152');
+
+
+
+
+r=bin(int(rnum));
+r=r[:1:-1];
+r+='0'*(18-len(r));
+
+rule=[i for x,i in zip(r,range(len(r))) if x=='1'];
+
+alias='b';
+
+ps=1;
+for a in rule:
+	if a>8 and ps:
+		alias+='s';
+		ps=0;
+	alias+=str((a)%9)
+
+golly.setalgo("QuickLife")
+# golly.note(alias)
+golly.setrule(alias);
