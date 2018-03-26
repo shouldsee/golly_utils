28a3d9c21b4ec7f5ac50f27df2a03a3ed81775bf
shouldsee <shouldsee.gem@gmail.com>
2018-03-26 03:55:40 +0100

    deal with '-' in KBs.py

--- mutate.py
+++ mutate.py
@@ -1,32 +1,30 @@
-## Written by Feng (shouldsee.gem@gmail.com) Feb 2017.
+## Written by Feng (shouldsee.gem@gmail.com) March 2018.
 import golly
 import KBs
-# rulestr=golly.getstring('NTCA number',golly.getclipstr()).split('_')[-1];
-alias = golly.getrule()
-rulestr = KBs.kb_2dntca().alias2rulestr(alias)
-bitstr = KBs.hex2bin(rulestr,102)
-# .lstrip('0b')
-
-# golly.note('%s'%len(bitstr))
-
 import random
-idx =  random.randint(0,102)
+
+
+# rulestr=golly.getstring('NTCA number',golly.getclipstr()).split('_')[-1];
+kb = KBs.kb_2dntca()
+# alias = golly.getrule()
+curr=golly.getrule().split(':')
+if len(curr)==1:
+	curr+=['']
+alias,suffix = curr
+
+rulestr = kb.alias2rulestr(alias)
+bitstr = KBs.hex2bin(rulestr,102)
+# assert KBs.bin2hex(bitstr)==rulestr
 bitlst = list(bitstr)
-dct = {'0':'1','1':'0'}
-bitlst[idx] = dct[bitlst[idx]]
-# golly.note(''.join(bitlst))
 
-bitstr = ''.join(bitlst)
-rulestr = hex(int(bitstr,2)).lstrip('0x').rstrip('L').zfill(26)
-alias = KBs.kb_2dntca().rulestr2alias(rulestr)
-# alias = KBs.kb_2dntca().rulestr2alias(rulestr)
+idx =  random.randint(0,102)
+flip = {'0':'1','1':'0'}
+bitlst[idx] = flip[bitlst[idx]]
+rulestr = KBs.bin2hex(''.join(bitlst))
+alias = kb.rulestr2alias(rulestr)
+
+
 golly.note(alias)
-curr=golly.getrule().split(':');
-if len(curr)>1:
-	curr=':'+curr[1];
-else:
-	curr='';
-golly.setalgo("QuickLife")
-golly.setrule(alias+curr);
-golly.setclipstr('\n'+alias);
+golly.setclipstr(alias)
 
+golly.setrule('%s:%s'%(alias,suffix))
